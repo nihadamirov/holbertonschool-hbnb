30c0a931adf58e4ca96ee025473fcbe26b344a7a
aminayusufova <158808697+aminayusufova@users.noreply.github.com>
2024-06-21 21:23:36 +0400

    creating city.py

---
+++ Models/city.py
@@ -0,0 +1,11 @@
+from .BaseClass import BaseClass
+
+class City(BaseClass):
+    def __init__(self, name, country_id):
+        super().__init__()
+        self.name = name
+        self.country_id = country_id
+        self.places = []
+
+    def add_place(self, place):
+        self.places.append(place)
