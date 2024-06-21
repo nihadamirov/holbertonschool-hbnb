50e529e8f651c6b5ea72b4cb31491c24150fc38d
aminayusufova <158808697+aminayusufova@users.noreply.github.com>
2024-06-21 21:00:18 +0400

    creating review.py

---
+++ Models/review.py
@@ -0,0 +1,9 @@
+from .BaseClass import BaseClass
+
+class Review(BaseClass):
+    def __init__(self, place_id, user_id, text, rating):
+        super().__init__()
+        self.place_id = place_id
+        self.user_id = user_id
+        self.text = text
+        self.rating = rating
