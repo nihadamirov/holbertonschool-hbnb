6369e2c49ed68140e5e6c97b2d33438dbd1dc1f2
aminayusufova <158808697+aminayusufova@users.noreply.github.com>
2024-06-23 13:36:36 +0400

    updating main.py

--- app/main.py
+++ app/main.py
@@ -3,12 +3,14 @@ from app.api.v1.users import user_api
 from app.api.v1.countries import country_api
 from app.api.v1.cities import city_api
 from app.api.v1.reviews import review_api
+from app.api.v1.amenities import amenity_api
 
 app = Flask(__name__)
 
 app.register_blueprint(user_api, url_prefix='/api/v1')
 app.register_blueprint(country_api, url_prefix='/api/v1')
 app.register_blueprint(city_api, url_prefix='/api/v1')
+app.register_blueprint(amenity_api, url_prefix='/api/v1')
 app.register_blueprint(review_api, url_prefix='/api/v1')
 
 if __name__ == "__main__":
